5f63124eef32dbe5c307aa075bc780fc5a562091
satxnine <fnuraini503@gmail.com>
2023-01-16 08:05:22 +0700

    update

--- index.php
+++ index.php
@@ -1,16 +1,16 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="//cdn.datatables.net/1.10.20/css/jquery.dataTables.min.css">
     <title>Data Siswa</title>
-</head>
-<body>
+  </head>
 
-<div class="container" style="margin-top: 80px">
+  <body>
+
+    <div class="container" style="margin-top: 80px">
       <div class="row">
         <div class="col-md-12">
           <div class="card">
@@ -64,5 +64,5 @@
           $('#myTable').DataTable();
       } );
     </script>
-</body>
+  </body>
 </html>
